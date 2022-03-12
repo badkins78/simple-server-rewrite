e48252661ceb965621343ff30ac5d66dc02504d1
badkins78 <72710121+badkins78@users.noreply.github.com>
2022-03-12 09:47:14 -0600

    Add files via upload

---
+++ server_clone.js
@@ -0,0 +1,86 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const querystring = require("querystring");
+const figlet = require("figlet");
+
+const server = http.createServer((req, res) => {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  switch (page) {
+    case "/":
+      fs.readFile("index.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+      break;
+    case "/otherpage":
+      fs.readFile("otherpage.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+      break;
+    case "/otherotherpage":
+      fs.readFile("otherotherpage.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+      break;
+    case "/api":
+      switch ("student" in params) {
+        case params["student"] == "leon":
+          res.writeHead(200, { "Content-Type": "application/json" });
+          objToJson = {
+            name: "leon",
+            status: "Boss Man",
+            currentOccupation: "Baller",
+          };
+          res.end(JSON.stringify(objToJson));
+          break;
+
+        case params["student"] != "leon":
+          res.writeHead(200, { "Content-Type": "application/json" });
+          objToJson = {
+            name: "unknown",
+            status: "unknown",
+            currentOccupation: "unknown",
+          };
+          res.end(JSON.stringify(objToJson));
+
+          break;
+      }
+      break;
+
+    case "/css/style.css":
+      fs.readFile("css/style.css", function (err, data) {
+        res.write(data);
+        res.end();
+      });
+
+      break;
+
+    case "/js/main.js":
+      fs.readFile("js/main.js", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/javascript" });
+        res.write(data);
+        res.end();
+      });
+      break;
+
+    default:
+      figlet("404!!", function (err, data) {
+        if (err) {
+          console.log("Something went wrong...");
+          console.dir(err);
+          return;
+        }
+        res.write(data);
+        res.end();
+      });
+  }
+});
+server.listen(8000);
